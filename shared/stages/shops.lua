39e4026338077dfa1f4a4ecb7e314199b53645fb
Vurado-official <vladlavin5@gmail.com>
2025-04-24 09:17:53 +0300

    update again

--- shared/stages/shops.lua
+++ shared/stages/shops.lua
@@ -2,9 +2,56 @@ local x = -700
 local y = -165
 
 function onCreate()
-    makeLuaSprite("e","bgsongs/bbsfc/shop",x,-900)
-    setScrollFactor('e', 1, 1);
+    createInstance('e', 'flixel.addons.display.FlxBackdrop', {nil, 0x01, 0, 0})
+    loadGraphic('e', 'bgsongs/bbsfc/bg')
     scaleObject('e', 2.8, 2.8);
+    setProperty('e.velocity.x', -100)
+    setProperty('e.y', -500)
 
-    addLuaSprite("e",false)
+
+    createInstance('e1', 'flixel.addons.display.FlxBackdrop', {nil, 0x01, 100, 100})
+    loadGraphic('e1', 'bgsongs/bbsfc/cloud')
+    scaleObject('e1', 2.8, 2.8);
+    setProperty('e1.velocity.x', -500)
+    setProperty('e1.y', -300)
+
+    createInstance('31', 'flixel.addons.display.FlxBackdrop', {nil, 0x01, 100, 100})
+    loadGraphic('31', 'bgsongs/bbsfc/cloud')
+    scaleObject('31', 2.8, 2.8);
+    setProperty('31.velocity.x', -300)
+    setProperty('31.y', -100)
+
+    createInstance('312', 'flixel.addons.display.FlxBackdrop', {nil, 0x01, 0, 0})
+    loadGraphic('312', 'bgsongs/bbsfc/smoke')
+    scaleObject('312', 2.8, 2.8);
+    setProperty('312.velocity.x', -300)
+    setProperty('312.y', -1000)
+
+
+
+    makeLuaSprite("1","bgsongs/bbsfc/poll",x, -1000)
+    setScrollFactor('1', 1, 1);
+    scaleObject('1', 2.8, 2.8);
+
+    makeLuaSprite("4","bgsongs/bbsfc/smoke",x, -1000)
+    setScrollFactor('4', 1, 1);
+    scaleObject('4', 2.8, 2.8);
+
+    makeLuaSprite("2","bgsongs/bbsfc/sexshop",x, -1000)
+    setScrollFactor('2', 1, 1);
+    scaleObject('2', 2.8, 2.8);
+
+
+    makeLuaSprite("3","bgsongs/bbsfc/lest",x, -1000)
+    setScrollFactor('3', 1, 1);
+    scaleObject('3', 2.8, 2.8);
+
+
+    addInstance('e', false)
+    addLuaSprite("31",false)
+    addLuaSprite("312",false)
+    addInstance('e1', false)
+    addLuaSprite("1",false)
+    addLuaSprite("2",false)
+    addLuaSprite("3",false)
 end
