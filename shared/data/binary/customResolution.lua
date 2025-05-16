03c7643e5952ba27ee251b1c7474302248567675
Vurado-official <vladlavin5@gmail.com>
2025-05-16 20:55:47 +0300

    remove songs

--- shared/data/binary/customResolution.lua
+++
@@ -1,6 +0,0 @@
-function onCreate()
-    setPropertyFromClass("openfl.Lib", "application.window.resizable", false)
-end
-function onDestroy()
-    setPropertyFromClass("openfl.Lib", "application.window.resizable", true)
-end
