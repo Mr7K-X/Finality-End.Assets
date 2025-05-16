1549d0ce252b62072b2118f6241cb87f30198a05
Vurado-official <vladlavin5@gmail.com>
2025-05-16 20:45:21 +0300

    shitty events delete

--- shared/events/playVideo.lua
+++
@@ -1,7 +0,0 @@
-function onEvent(name, value1, value2)
-     if name == 'cut1' then
-          startVideo(value1, value2)
-          return Function_Stop
-     end
-     return Function_Continue
-end
